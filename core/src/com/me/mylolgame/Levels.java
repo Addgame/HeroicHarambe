3458794d8f0ba20d6be4e6ced73fa608387ae22a
AlexP <alp320@lehigh.edu>
2016-09-16 15:54:27 -0400

    Added more platform things

--- core/src/com/me/mylolgame/Levels.java
+++ core/src/com/me/mylolgame/Levels.java
@@ -119,7 +119,7 @@ public class Levels implements ScreenManager {
             Control.addThrowButton(20, 7, 50, 50, "", h, 100, 3, 1.5f, 30, 0);
             Level.setCameraChase(h);
             h.setJumpImpulses(0, 14);
-            Control.addJumpButton(110, 7, 50, 50, "red.png", h);
+            Control.addJumpButton(110, 7, 50, 50, "HarambeArt.Jump.png", h);
 
             Control.addLeftButton(500,7, 50, 50, "", 20, h);
             Control.addRightButton(590,7, 50, 50, "", 20, h);
