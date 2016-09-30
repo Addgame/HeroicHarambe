70624f9a88872212312098c5fec5aea02ee3955c
Adam <agw320@lehigh.edu>
2016-09-30 14:54:58 -0400

    added comments for ScoreHack changed Actor.setPhysics to return the Actor

--- core/src/edu/lehigh/cse/lol/ScoreHack.java
+++ core/src/edu/lehigh/cse/lol/ScoreHack.java
@@ -3,7 +3,14 @@ package edu.lehigh.cse.lol;
 /**
  * Created by CSE User on 9/30/2016.
  */
+
+/*
+ * Added for access to enemies created
+ */
 public class ScoreHack {
+    /*
+     * Gets enemies created
+     */
     public static int getEnemiesCreated() {
         return Lol.sGame.mCurrentLevel.mScore.mEnemiesCreated;
     }
